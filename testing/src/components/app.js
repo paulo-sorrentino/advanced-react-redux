b15f2fd5d59a90f26ff91342f25a177ea4e689ba
Paulo Sorrentino <inshideru@gmail.com>
2018-05-07 23:28:45 -0300

    Testing

---
+++ testing/src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import CommentBox from '../../src/components/comment_box';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <CommentBox />
+      </div>
+    );
+  }
+}
